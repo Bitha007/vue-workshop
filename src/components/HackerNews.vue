7e0bbbfaf6a1ef735748d1439a0a991b16dd07e9
Bitha007 <bibekthapa922@gmail.com>
2019-09-29 16:45:59 +0545

    add Modal

--- src/components/HackerNews.vue
+++ src/components/HackerNews.vue
@@ -4,56 +4,38 @@
       <div class="heading">
         <h4 class="title">Netflix Ratings</h4>
       </div>
-      <div class="search">
-        <input v-model="title" type="text" class="form-control" placeholder="Search by title" />
-      </div>
-      <div class="content">
-        <div v-if="loading">
-          <p>Loading news</p>
-        </div>
-        <div v-else>
-          <div v-if="error">
-            <p>{{error}}</p>
-          </div>
-          <ul v-else class="list">
-            <li v-for="(news, index) in stories" :key="index">
-              <template v-if="news.title">
-                <a :href="news.url">{{news.title}}</a>
-                <span>By: {{news.author}}</span>
-              </template>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <Search @title="handleTitleChange" />
+      <Stories :stories="stories" :loading="loading"   />
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-
+import Stories from "./Stories";
+import Search from "./Search";
 export default {
+
   name: "HackerNews",
   data: function() {
     return {
       loading: true,
       error: null,
       stories: [],
-      title: ""
     };
   },
   created: async function() {
     this.getData();
   },
   methods: {
-    async getData() {
+    async getData(title ) {
       this.loading = true;
       try {
         const response = await axios.get(
           "http://hn.algolia.com/api/v1/search",
           {
             params: {
-              query: this.title
+              query: title
             }
           }
         );
@@ -63,79 +45,21 @@ export default {
       } finally {
         this.loading = false;
       }
+    },
+    async handleTitleChange(title){
+      this.getData(title);
     }
   },
   watch: {
     title: function(value) {
+      console.log(value)
       this.getData();
     }
+  },
+  components:{
+    Stories,
+    Search
   }
 };
 </script>
 
-<style scoped lang="scss">
-.container {
-  margin: 0 auto;
-  max-width: 768px;
-}
-
-.card {
-  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
-  padding: 24px;
-}
-
-.list {
-  > li {
-    &:not(:last-child) {
-      margin-bottom: 18px;
-    }
-    > a {
-      color: #0a5b8c;
-      display: block;
-      margin-bottom: 6px;
-    }
-
-    > span {
-      color: rgba(#3b4242, 0.7);
-      font-size: 12px;
-    }
-  }
-}
-
-.btn {
-  color: #fff;
-  cursor: pointer;
-  background-color: #117a8b;
-  border-color: #10707f;
-  border: 1px solid transparent;
-  padding: 6px 12px;
-  border-radius: 6px;
-  transition: all 0.1s ease-in;
-  &:hover {
-    background-color: #138496;
-    border-color: #117a8b;
-  }
-}
-
-.heading {
-  margin-bottom: 12px;
-
-  .title {
-    font-size: 18px;
-    font-weight: 600;
-  }
-}
-.search {
-  margin-bottom: 24px;
-  .form-control {
-    background-color: transparent;
-    border: none;
-    border-bottom: 1px solid #ced4da;
-    border-radius: 0;
-    outline: 0;
-    box-shadow: none;
-    padding: 6px 0;
-    width: 100%;
-  }
-}
-</style>
---
+++ src/components/Stories.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="content">
+    <div v-if="loading">
+      <p>Loading news</p>
+    </div>
+    <div v-else>
+      <div v-if="error">
+        <p>{{error}}</p>
+      </div>
+      <ul v-else class="list">
+        <li v-for="(news, index) in stories" :key="index">
+          <template v-if="news.title">
+            <a :href="news.url">{{news.title}}</a>
+            <span>By: {{news.author}}</span>
+          </template>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Stories",
+  props:{
+    stories:Array, 
+    loading:Boolean,
+    error:String
+  }
+};
+</script>
+
+<style  scoped lang="scss">
+.container {
+  margin: 0 auto;
+  max-width: 768px;
+}
+
+.card {
+  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
+  padding: 24px;
+}
+
+.list {
+  > li {
+    &:not(:last-child) {
+      margin-bottom: 18px;
+    }
+    > a {
+      color: #0a5b8c;
+      display: block;
+      margin-bottom: 6px;
+    }
+
+    > span {
+      color: rgba(#3b4242, 0.7);
+      font-size: 12px;
+    }
+  }
+}
+
+.btn {
+  color: #fff;
+  cursor: pointer;
+  background-color: #117a8b;
+  border-color: #10707f;
+  border: 1px solid transparent;
+  padding: 6px 12px;
+  border-radius: 6px;
+  transition: all 0.1s ease-in;
+  &:hover {
+    background-color: #138496;
+    border-color: #117a8b;
+  }
+}
+
+.heading {
+  margin-bottom: 12px;
+
+  .title {
+    font-size: 18px;
+    font-weight: 600;
+  }
+}
+  
+</style>
